0127a281c9baf86274e7f891c72ce8eb1a9ccf41
Vaughan Jackson <vaughanjackson2001@yahoo.com>
2020-03-28 15:54:14 +0000

    Move Wheel to its own dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:word_wheels/wheel.dart';
 
 void main() {
   runApp(CupertinoExample());
@@ -87,43 +87,3 @@ class _ExamplePageState extends State<ExamplePage> {
   }
 }
 
-class Wheel extends StatefulWidget {
-  final String wheelKey;
-  final String characters;
-
-  Wheel(this.wheelKey, this.characters);
-
-  @override
-  State<StatefulWidget> createState() => _WheelState();
-
-}
-
-class _WheelState extends State<Wheel> {
-  int selectedItem = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    final List<Widget> buttons = [];
-    widget.characters.runes.forEach((int rune) {
-      var character = new String.fromCharCode(rune);
-      buttons.add(MaterialButton(
-        key: Key('$widget.wheelKey.$character'),
-        child: Text(
-          character,
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-      ));
-    });
-    return CupertinoPicker(
-      key: Key(widget.wheelKey),
-      magnification: 1.5,
-      backgroundColor: Colors.black87,
-      children: buttons,
-      itemExtent: 50, //height of each item
-      looping: false,
-      onSelectedItemChanged: (int index) {
-        selectedItem = index;
-      },
-    );
-  }
-}
---
+++ lib/wheel.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Wheel extends StatefulWidget {
+  final String wheelKey;
+  final String characters;
+
+  Wheel(this.wheelKey, this.characters);
+
+  @override
+  State<StatefulWidget> createState() => _WheelState();
+
+}
+
+class _WheelState extends State<Wheel> {
+  int selectedItem = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> buttons = [];
+    widget.characters.runes.forEach((int rune) {
+      var character = new String.fromCharCode(rune);
+      buttons.add(MaterialButton(
+        key: Key('$widget.wheelKey.$character'),
+        child: Text(
+          character,
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ));
+    });
+    return CupertinoPicker(
+      key: Key(widget.wheelKey),
+      magnification: 1.5,
+      backgroundColor: Colors.black87,
+      children: buttons,
+      itemExtent: 50, //height of each item
+      looping: false,
+      onSelectedItemChanged: (int index) {
+        selectedItem = index;
+      },
+    );
+  }
+}
