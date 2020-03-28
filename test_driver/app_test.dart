e066b15cffda816ad39df938c573ce81ba966616
Vaughan Jackson <vaughanjackson2001@yahoo.com>
2020-03-28 17:49:12 +0000

    Align integration tests with new widget classes based implementation.

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -19,66 +19,61 @@ void main() {
     });
 
     test('starts game', () async {
-      // final kaishi = find.text('开始！'); // TODO not I18n compatible
       final kaishi = find.byValueKey("开始！");
 
       await driver.tap(kaishi);
-
     });
 
     test('test wheel1', () async {
-      final wheel1 = find.byValueKey("wheel1");
+      final wheel1 = find.byValueKey('wheel1');
 
       await driver.tap(wheel1);
-
     });
 
-//    test('test wheel1.你', () async {
-//      final ni1 = find.byValueKey("wheel1.你");
-//
-//      await driver.tap(ni1);
-//
-//    });
-
-    test('verifies wheels contain a specific item', () async {
+    scrollWheelToCharacter(
+        final FlutterDriver driver,
+        final String wheelKey,
+        final String characterSought) async {
       // Create two SerializableFinders and use these to locate specific
       // widgets displayed by the app. The names provided to the byValueKey
       // method correspond to the Keys provided to the widgets in step 1.
-//      final wheelsFinder = find.byValueKey('wheels');
-      final wheel1 = find.byValueKey("wheel1");
-//      final wheel1 = find.byType('CupertinoPicker');
-//      final wheel2Finder = find.byValueKey('wheel2');
-//      final wheel3Finder = find.byValueKey('wheel3');
-//      final wheel4Finder = find.byValueKey('wheel4');
-//      final wheel5Finder = find.byValueKey('wheel5');
-
-//      final ni1 = find.byValueKey('你');
-//      final hao1 = find.byValueKey('好');
-//      final ma1 = find.byValueKey('吗');
-//      final ni1 = find.byValueKey("wheel1.你");
-      final q = find.byValueKey("wheel1.?");
-
+      final wheel = find.byValueKey(wheelKey);
+      final character = find.byValueKey('$wheelKey.$characterSought');
 
       await driver.scrollUntilVisible(
         // Scroll through the list
-        wheel1,
+        wheel,
         // Until finding this item
-        q,
+        character,
         // To scroll down the list, provide a negative value to dyScroll.
         // Ensure that this value is a small enough increment to
         // scroll the item into view without potentially scrolling past it.
         //
         // To scroll through horizontal lists, provide a dxScroll
         // property instead.
-        dyScroll: 10.0,
+        dyScroll: -10.0,
       );
 
       // Verify that the item contains the correct text.
-      final text = find.byValueKey('wheel1.?.text');
+      final text = find.byValueKey('$wheelKey.$characterSought.text');
       expect(
         await driver.getText(text),
-        '?',
+        characterSought,
       );
+    }
+
+    test('Verify can scroll first wheel to last character', () async {
+      await scrollWheelToCharacter(driver, 'wheel1', '里');
     });
+
+    test('Verify can scroll second wheel to last character', () async {
+      await scrollWheelToCharacter(driver, 'wheel2', '里');
+    });
+
+    test('Verify can scroll third wheel to last character', () async {
+      await scrollWheelToCharacter(driver, 'wheel3', '里');
+    });
+
   });
+
 }
