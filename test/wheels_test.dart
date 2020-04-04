e1a48f55d94f6218ea273cbd97201609f2860ffc
Vaughan Jackson <vaughanjackson2001@yahoo.com>
2020-04-04 13:16:55 +0100

    First successful widget tests, with Directionality rendering workaround.

---
+++ test/wheels_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:word_wheels/wheels.dart';
+
+// Tests the Wheels widget.
+void main() {
+  testWidgets('Wheels widget behaves as expected', (WidgetTester tester) async {
+
+    // Build our widget and trigger a frame.
+    // TODO To avoid 'Failed assertion: line 530 pos 12: 'direction != null':
+    //  is not true' or similar here had to wrap the Wheel widget under test in
+    //  a Directionality element. Found this suggestion here:
+    // https://github.com/flutter/flutter/issues/21266. Does this really mean
+    // the Wheels widget itself should incorporate a Directionality element or
+    // similar?
+    await tester.pumpWidget(Directionality(
+        textDirection: TextDirection.ltr,
+        child: Wheels([
+          '你的中文在这里',
+          '我的中文在这里',
+          '她的中文在这里'
+        ]))
+    );
+
+    // TODO Assert more than widget can be rendered without blowing up!
+//    // Verify that our counter starts at 0.
+//    expect(find.text('0'), findsOneWidget);
+//    expect(find.text('1'), findsNothing);
+//
+//    // Tap the '+' icon and trigger a frame.
+//    await tester.tap(find.byIcon(Icons.add));
+//    await tester.pump();
+//
+//    // Verify that our counter has incremented.
+//    expect(find.text('0'), findsNothing);
+//    expect(find.text('1'), findsOneWidget);
+  });
+}
